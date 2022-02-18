0d0c332cbdd254abb61ce5ba213fcd6060e5509f
Roadg14 <douglasgc12362@gmail.com>
2022-02-18 16:45:34 -0300

    first commit

--- js/Style.js
+++ js/Style.js
@@ -1,4 +1,4 @@
-function showPassword() {
+function showPassword () {
     const eye = document.getElementById('eye');
     const eyeSlash = document.getElementById('eye-slash');
     const fieldPassword = document.getElementById('field-password');
@@ -6,12 +6,16 @@ function showPassword() {
     if (eye.style.display === 'none') {
         eye.style.display = 'block';
         eyeSlash.style.display = 'none';
-        eyeSlash.style.display = 'block';
         fieldPassword.type = 'text'
     } else {
         eye.style.display = 'none';
-        eyeSlash.display = 'block';
-        fieldPassword.type = 'text';
+        eyeSlash.style.display = 'block';
+        fieldPassword.type = 'password';
 
     }
-}
+};
+
+document.getElementById('btn-login').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('Logado!');
+});
